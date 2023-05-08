ffedf7d04102043e330048be14f1c310abcdabe6
Sudip Karmakar <105915597+sudip40@users.noreply.github.com>
2023-05-08 12:32:30 +0530

    Add files via upload

---
+++ 01_where_is_the_gods.py
@@ -0,0 +1,31 @@
+print('''‘Where is God now?’, I have been asked.
+‘Where is God when his children die?
+Where is God when all this evil is unmasked?
+
+Where is God when families are torn apart
+By their anxieties, and by the rule of law?
+Where is your God when I lost heart?’
+
+In truth, I was burdened by these words:
+Where was my God; the very God I loved?
+So I set out in search of God and travelled onwards.
+
+And through my life, I did see hardship and death,
+Fraud and hatred, separation and misery.
+So I called out to God, all short of breath.
+
+‘O Lord, where are you now, are you unmoved?
+This world around me has been burdened!
+Why has all this wasteful evil not been removed?’
+…………….
+And the words of the good psalmist cleared the air,
+“If I ascend up into heaven, thou art there:
+if I make my bed in hell, behold, thou art there. *
+
+For not even death and hardship can sever our ties.
+And if through prosperity, I have come to know You,
+O Lord, let me learn to love You through my demise.
+…………….
+As I write these words, I still do not grasp Your ways.
+But full of hope, in Your heart I confide my own.
+Help us O God, so in our grief we may bear Your grace.''')
