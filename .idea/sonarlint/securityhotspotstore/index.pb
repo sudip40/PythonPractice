86331b525c1333809bd04e548b3def41ec11a68a
Sudip Karmakar <105915597+sudip40@users.noreply.github.com>
2024-06-30 19:54:06 +0530

    started hang_man project

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+V
+&Day1(variables)/band_name_generator.py,d/9/d9c39d4b42d0105accca95b4d7902c3d78436569
+G
+01_where_is_the_gods.py,7/f/7fd58162b117a0dcdf293d27687749ff1b140513
+R
+"Day3(If-elif-else)/rollercoster.py,9/c/9c92b4260e83dc34effe886ad02d3f968c791863
+k
+;Day2(data types & mathematical operators)/tip_calculator.py,6/3/63fdc8500c82345b6c81ab5d33e1cf489cac795a
+J
+Day7(Hang_Man)/hang_man.py,5/f/5ffe3ce60c1e64a044e0029829c5fe167c24c572
---
+++ Day7(Hang_Man)/hang_man.py
@@ -0,0 +1,32 @@
+import random
+
+word_list = [
+    "apple",
+    "banana",
+    "orange",
+    "grape",
+    "strawberry",
+    "melon",
+    "peach",
+    "kiwi",
+    "pineapple",
+    "blueberry",
+    "mango",
+    "pear",
+    "plum",
+    "cherry",
+    "raspberry",
+    "apricot",
+    "blackberry",
+    "watermelon",
+    "lemon",
+    "lime"
+]
+
+random_number = random.randint(0,20)
+random_word = word_list[random_number]
+print(random_word)
+
+# TODO as the user to guess the letter and assign their ans to a variable called guess. Make guess lowercase.
+
+# TODO check if the guessed letter is one of the choosen word's letter or not
